92f25a3a2ffc638bd5a0b339002574e38547db8c
Chernogorsky Fedor <fyodorchernogor@gmail.com>
2019-05-04 14:02:40 +0300

    task10 solved

--- task10/test/TestBasics.hs
+++ task10/test/TestBasics.hs
@@ -11,23 +11,50 @@ testsBasics = testGroup "Unit tests for Basics tasks"
     [testCase "head' works on non-empty list" $
         head' [1,2,3] @?= 1
 
+    , testCase "head' works on endless list" $
+        head' [1..] @?= 1
+
     , testCase "tail' works on non-empty list too" $
         tail' [1,2,3] @?= [2,3]
+
+    , testCase "tail' works on empty list" $
+        tail' ([]::[Int]) @?= []
 
     , testCase "take' takes 1 element from 3-element list" $
         take' 1 [1,2,3] @?= [1]
 
+    , testCase "take' works on empty list" $
+        take' 3 ([]::[Int]) @?= []
+
+    , testCase "take' takes 10 elements from endless list" $
+        take' 10 [1..] @?= [1..10]
+
     , testCase "drop' drops 1 element from 3-element list" $
         drop' 1 [1,2,3] @?= [2,3]
 
+    , testCase "drop' works on empty list" $
+        drop' 10 ([]::[Int]) @?= []
+    
+    , testCase "drop' drops 10 elements from 30-element list" $
+        drop' 10 [1..30] @?= [11..30]
+
     , testCase "filter' selects only even numbers from 0 to 10" $
         filter' even [0..10] @?= [0,2..10]
+    
+    , testCase "filter' selects only even numbers form endless list" $
+        take' 100 (filter' even [1..]) @?= take' 100 [2,4..]
 
     , testCase "foldl'' can be used for finding sum of elements" $
         foldl'' (+) 0 [1,2,3] @?= 6
 
+    , testCase "foldl'' can be used for finding product of elements" $
+        foldl'' (*) 1 [1,2,3] @?= 6
+
     , testCase "concat' works on finite lists as expected" $
         concat' [1,2,3] [4,5,6] @?= [1..6]
+
+    , testCase "concat' works on finite and infinite list" $
+        take' 100 (concat' [1,2] [3..]) @?= take' 100 [1..] 
 
     , testCase "quickSort actualy sorts the list" $
         quickSort' [5,2,3,4,1] @?= [1..5]
--- task10/test/TestRobots.hs
+++ task10/test/TestRobots.hs
@@ -9,9 +9,36 @@ main = defaultMain testsRobots
 testsRobots :: TestTree
 testsRobots = let
         walter = robot "Walter" 50 50
+        jesse = robot "Jesse" 30 10
+        gus = robot "Gus" 100 (-100)
+        mike = robot "Mike" 70 50
     in testGroup "Unit tests for Robots task"
         [ testCase "Test for getName" $
             getName walter @?= "Walter"
         , testCase "Test for printRobot" $
             printRobot walter @?= "Walter, attack: 50, health: 50"
+        , testCase "Test for getAttack" $
+            getAttack jesse @?= 30
+        , testCase "Test for getHealth" $
+            getHealth mike @?= 50
+        , testCase "Test for setName" $
+            setName "Heisenberg" walter @?= ("Heisenberg", 50, 50)
+        , testCase "Test for setAttack" $
+            setAttack 500 walter @?= ("Walter", 500, 50)
+        , testCase "Test for setHealth" $
+            setHealth 0 gus @?= ("Gus", 100, 0)
+        , testCase "Test for damage" $
+            damage walter 50 @?= ("Walter", 50, 0)
+        , testCase "Test for isAlive" $
+            isAlive jesse @?= True
+        , testCase "Test for isAlive" $
+            isAlive gus @?= False
+        , testCase "Fight between 2 robots" $
+            fight mike walter @?= ("Walter", 50, -20)
+        , testCase "Fight dead and alive robots" $
+            fight gus walter @?= ("Walter", 50, 50)
+        , testCase "Test for 3 rounds" $
+            threeRoundFight jesse walter @?= ("Walter", 50, 20)
+        , testCase "Test for survivors" $
+            survivors @?= [("Byvaliy", 100, 100)]
         ]
