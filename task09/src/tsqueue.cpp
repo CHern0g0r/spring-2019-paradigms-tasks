09f1a9a2c06b85ddc48330a6d4a548dad45076a3
Chernogorsky Fedor <fyodorchernogor@gmail.com>
2019-05-22 11:43:23 +0300

    Remove mutex lock, change free order, change non_empty

--- task09/src/tsqueue.cpp
+++ task09/src/tsqueue.cpp
@@ -2,31 +2,27 @@
 
 void threadsafe_queue_init(ThreadsafeQueue *q) {
     pthread_mutex_init(&q->mutex, NULL);
-    pthread_cond_init(&q->cond, NULL);
-    pthread_mutex_lock(&q->mutex);
+    pthread_cond_init(&q->non_empty, NULL);
     queue_init(&q->q);
-    pthread_mutex_unlock(&q->mutex);
 }
 
 void threadsafe_queue_destroy(ThreadsafeQueue *q) {
-    pthread_mutex_lock(&q->mutex);
     queue_destroy(&q->q);
-    pthread_mutex_unlock(&q->mutex);
+    pthread_cond_destroy(&q->non_empty);
     pthread_mutex_destroy(&q->mutex);
-    pthread_cond_destroy(&q->cond);
 }
 
 void threadsafe_queue_push(ThreadsafeQueue *q, void *data) {
     pthread_mutex_lock(&q->mutex);
     queue_push(&q->q, data);
-    pthread_cond_signal(&q->cond);
+    pthread_cond_signal(&q->non_empty);
     pthread_mutex_unlock(&q->mutex);
 }
 
 void *threadsafe_queue_wait_and_pop(ThreadsafeQueue *q) {
     pthread_mutex_lock(&q->mutex);
     while (queue_empty(&q->q))
-        pthread_cond_wait(&q->cond, &q->mutex);
+        pthread_cond_wait(&q->non_empty, &q->mutex);
 
     void *return_value = queue_pop(&q->q);
     pthread_mutex_unlock(&q->mutex);
--- task09/src/tsqueue_test.cpp
+++ task09/src/tsqueue_test.cpp
@@ -43,10 +43,10 @@ TEST_CASE("ThreadsafeQueue multithreaded ping-pong") {
     auto pinger = [](void *_qs) -> void * {
         ThreadsafeQueue *qs = static_cast<ThreadsafeQueue *>(_qs);
         for (int i = 0; i < PING_PONGS; i++) {
-            int number = 73;
-            threadsafe_queue_push(&qs[0], &number);
-            REQUIRE(threadsafe_queue_wait_and_pop(&qs[1]) == &number);
-            REQUIRE(number == 74);
+            int ping = 73;
+            threadsafe_queue_push(&qs[0], &ping);
+            REQUIRE(threadsafe_queue_wait_and_pop(&qs[1]) == &ping);
+            REQUIRE(ping == 74);
         }
         return nullptr;
     };
@@ -54,10 +54,10 @@ TEST_CASE("ThreadsafeQueue multithreaded ping-pong") {
     auto ponger = [](void *_qs) -> void * {
         ThreadsafeQueue *qs = static_cast<ThreadsafeQueue *>(_qs);
         for (int i = 0; i < PING_PONGS; i++) {
-            int *result =
+            int *pong =
                 static_cast<int *>(threadsafe_queue_wait_and_pop(&qs[0]));
-            ++*result;
-            threadsafe_queue_push(&qs[1], result);
+            ++*pong;
+            threadsafe_queue_push(&qs[1], pong);
         }
         return nullptr;
     };
